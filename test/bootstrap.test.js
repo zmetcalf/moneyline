734b385d55786c6bdf42d150d16b77b970ffe1de
Zachery Metcalf <zachery.metcalf@gmail.com>
2016-02-01 20:32:31 -0700

    Added in testing framework

---
+++ test/bootstrap.test.js
@@ -0,0 +1,19 @@
+var Sails = require('sails');
+
+before(function(done) {
+  // Increase the Mocha timeout so that Sails has enough time to lift.
+  this.timeout(5000);
+
+  Sails.lift({
+    // configuration for testing purposes
+  }, function(err, server) {
+    if (err) return done(err);
+    // here you can load fixtures, etc.
+    done(err, server);
+  });
+});
+
+after(function(done) {
+  // here you can clear fixtures, etc.
+  Sails.lower(done);
+});
---
+++ test/services/GameService.test.js
@@ -0,0 +1,9 @@
+describe('UserController', function() {
+
+  describe('#login()', function() {
+    it('should redirect to /mypage', function (done) {
+      done();
+    });
+  });
+
+});
