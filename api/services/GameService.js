c0761b894b4aed38ef41fb9f005b73b2cdf80658
Zachery Metcalf <zachery.metcalf@gmail.com>
2016-03-31 21:27:10 -0600

    Starting to pair with tests - fs returns string however done gets call too often

--- api/services/GameService.js
+++ api/services/GameService.js
@@ -5,7 +5,7 @@ module.exports = {
   updateGames: function(callback) {
     async.waterfall([
       function(_mainCb) { // get raw xml
-        request('http://sportsfeeds.bovada.lv/basic/NHL.xml', function(err, response, body) {
+        request.get('http://sportsfeeds.bovada.lv/basic/NHL.xml', function(err, response, body) {
           if(err) _mainCb(err);
           _mainCb(null, body);
         });
@@ -20,33 +20,39 @@ module.exports = {
       },
 
       function(xml, _mainCB) { // Game Loop
-        async.each(xml.Schedule.EventType.Date, function(game, _gameCb) {
+        async.each(xml.Schedule.EventType[0].Date, function(game, _gameCb) {
           async.waterfall([
             function(_innerGameCb) { // Check if game exists and is finished.
-
+              gameQuery = Game.findOne().where(
+                { 'gameId': game.ID }).exec(function(game) {
+                _innerGameCb(null);
+              });
             },
 
             function(_innerGameCb) { // Get or Create Teams
-
+              var home, away;
+              _innerGameCb(null, home, away);
             },
 
             function(home, away, _innerGameCb) { // Get or Create Game
-
+              var game;
+              _innerGameCb(null, home, away, game);
             },
 
             function(home, away, game, _innerGameCb) { // Check last datapoint - add if updated
-
+              _innerGameCb(null, home, away, game);
             },
 
             function(home, away, game, _innerGameCb) { // If game done add result
-
+              _innerGameCb(null);
             }
           ], function(err) {
             if(err) _gameCb(err);
-            _gameCb(null)
+            _gameCb(null);
           });
-
-        }
+        }, function(err) {
+          _mainCB(null);
+        });
       }
 
     ], function(err, result) {
--- test/services/GameService.test.js
+++ test/services/GameService.test.js
@@ -1,5 +1,22 @@
+var fs = require('fs'),
+    sinon = require('sinon'),
+    request = require('request');
+
 describe('GameService', function() {
   describe('updateGames', function() {
+    beforeEach(function(done) {
+      fs.readFile('test/services/nhl.xml', 'utf-8', function(err, file) {
+        sinon.stub(request, 'get', function(url, callback) {
+          return callback(err, file);
+        });
+        done();
+      });
+    });
+
+    afterEach(function() {
+      request.get.restore();
+    });
+
     it('should update games.', function (done) {
       GameService.updateGames(function(err, response) {
         console.log(response);
@@ -7,5 +24,4 @@ describe('GameService', function() {
       });
     });
   });
-
 });
